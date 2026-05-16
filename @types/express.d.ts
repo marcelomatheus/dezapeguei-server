ffff3d87f9f4d78b3e9f65d246bd4f1c94310765
marcelomatheus <marcelomatheusbr@gmail.com>
2026-05-15 23:08:22 -0300

    feat: extend Multer namespace with File interface for enhanced file handling

--- @types/express.d.ts
+++ @types/express.d.ts
@@ -2,6 +2,20 @@ import { Account, User } from '@prisma/client';
 
 declare global {
   namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
+    }
+
     interface Request {
       account?: Account;
       user?: User;
