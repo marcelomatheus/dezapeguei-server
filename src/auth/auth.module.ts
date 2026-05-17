8960fbfcbce881eae172fcbf571bf341211956a1
marcelomatheus <marcelomatheusbr@gmail.com>
2026-05-17 18:08:23 -0300

    feat: use forwardRef for circular dependency resolution in Auth and Users modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { SupabaseModule } from '../supabase/supabase.module';
@@ -7,7 +7,7 @@ import { SupabaseAuthGuard } from './guards/user-auth.guard';
 import { WsAuthGuard } from './guards/ws-auth.guard';
 
 @Module({
-  imports: [SupabaseModule, UsersModule],
+  imports: [SupabaseModule, forwardRef(() => UsersModule)],
   controllers: [AuthController],
   providers: [AuthService, SupabaseAuthGuard, WsAuthGuard],
   exports: [AuthService, SupabaseAuthGuard, WsAuthGuard],
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { StorageModule } from '../storage/storage.module';
+import { SupabaseModule } from '../supabase/supabase.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [StorageModule],
+  imports: [StorageModule, SupabaseModule, forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
